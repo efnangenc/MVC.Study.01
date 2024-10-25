f5f7469d0b65f1e589c00ee09777aa10b1f9ee47
Efnan <genc.efnan4@gmail.com>
2024-10-25 17:29:56 +0300

    First Initialize

---
+++ MVC.Study.01/Controllers/deneme.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC.Study._01.Controllers
+{
+    public class deneme : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MVC.Study.01/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVC.Study._01.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
